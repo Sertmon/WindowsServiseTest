0fc48b16c381f16a6847d1bed26ba5966c8c14c5
Igor Yurin <korin.igor31@yandex.ru>
2023-12-09 01:34:04 +0300

    gdfg

--- WindowsServiseTest/Service1.cs
+++ WindowsServiseTest/Service1.cs
@@ -22,7 +22,7 @@ namespace WindowsServiseTest
         {
             WriteToFile("Service is started at " + DateTime.Now);
             timer.Elapsed += new ElapsedEventHandler(OnElapsedTime);
-            timer.Interval = 5000; //number in milisecinds  
+            timer.Interval = 5000; //number in milisecinds ff 
             timer.Enabled = true;
         }
         protected override void OnStop()
